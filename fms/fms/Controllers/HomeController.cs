049bbe285f5cf71357b86a2976506189aabfe9c1
ErenBarut <erenbarut5566@gmail.com>
2024-01-10 21:46:48 +0300

    DosyaYonetimSistemi
    20221129066

---
+++ fms/fms/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using fms.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using fms.Models;
+using fms.ViewModels;
+
+namespace fms.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ fms/fms/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using fms.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace fms.Models
+{
+    public class AppDbContext : IdentityDbContext<AppUser, AppRole, string>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+    }
+}
